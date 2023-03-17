4a4ba72c4fd6a455f7fac564a7aee455fdee4e80
Jaqueline Medeiros <jaqueline.ms@outlook.com.br>
2023-03-17 20:34:22 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle('light')
+
+    const img = document.querySelector(".profile img")
+
+    if (html.classList.contains('light')) {
+        img.setAttribute('src', './assets/perfil-light.jpg')
+    } else {
+        img.setAttribute('src', './assets/perfil.jpeg')
+    }
+}
